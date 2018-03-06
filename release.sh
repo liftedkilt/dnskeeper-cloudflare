a829f39f4ac9aa40349120192614d89694953f81
William Forsyth <william.forsyth@liferay.com>
2018-03-06 13:57:58 -0800

    Change version numbering to comply with PyPi

---
+++ release.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Cuts new releases
+
+cd "$( dirname "${BASH_SOURCE[0]}")"
+
+currentCodeVersion=$(python setup.py --version)
+currentGitTag=$(git tag | tail -n 1 )
+
+read -p "Release will be cut and git tag created based on what is currently committed. Proceed? [y/N]: " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Aborting. Commit all files to git before running release.shS"
+    exit 1
+fi
+
+echo "Current Git Tag: " $currentGitTag
+echo "Current Code version: " $currentCodeVersion
+
+#
+# Ask for new version.
+#
+
+read -p "Please specify new version: " newVersion
+
+#
+# change version in code.
+#
+
+sed -i "s/__version__ = \"${currentCodeVersion}\"/__version__ = \"${newVersion}\"/" dnskeeper/dnskeeper.py
+
+#
+# Create git tag for specified version.
+#
+
+git tag -a newVersion
+
+#
+# Create distribution files.
+#
+
+python setup.py bdist_wheel
+python setup.py sdist
+
+#
+# Push to PyPi
+#
+
+read -p "Push build to PyPi? [y/N]: " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Aborting. Manually push with twine upload ./dist/*"
+    exit 1
+fi
+
+twine upload ./dist/*
