439c4b044b6188caa7d93c9ba53eb00c893dd0b3
William Forsyth <william.forsyth@liferay.com>
2018-03-06 13:17:42 -0800

    Initial Push

---
+++ dnskeeper-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#
+# Convenience wrapper for running dnskeeper directly from source tree.
+#
+
+from dnskeeper.dnskeeper import main
+
+if __name__ == '__main__':
+    main()
